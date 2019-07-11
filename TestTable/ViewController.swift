c8867f7bb58e41d09214df55ceaa749381c24518
denisKa <denis.karpenko@numbrs.com>
2019-07-11 18:28:37 +0200

    Initial Commit

---
+++ TestTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTable
+//
+//  Created by Denis Karpenko on 2019-07-11.
+//  Copyright © 2019 Denis Karpenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
